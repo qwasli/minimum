d55026c8bd65dce19df915fc7912c58669c73319
Raffael Bachmann <raffael.bachmann@a4d.ch>
2013-12-12 21:19:00 +0100

    adding styles and image for double sized date field

--- src/main/java/ch/meemin/minimum/utils/Props.java
+++ src/main/java/ch/meemin/minimum/utils/Props.java
@@ -8,4 +8,5 @@ public class Props {
 	public static final Image ICON_NOT_OK = new Image("", new ThemeResource("icons/128/notOK2.png"));
 	public static final Image ICON_WARN = new Image("", new ThemeResource("icons/128/warn2.png"));
 	public static final String MINIMUMBUTTON = "minimumbutton";
+	public static final String MINIMUMDATEFIELD = "minimumdatefield";
 }
