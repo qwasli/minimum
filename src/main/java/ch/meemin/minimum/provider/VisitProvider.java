59b66f25c88d3541b9278c5a108a1a94ae364ee6
Raffael Bachmann <raffael.bachmann@a4d.ch>
2014-04-14 18:11:48 +0200

    adding VisitProvider class and functions to prepare statistics

--- src/main/java/ch/meemin/minimum/provider/VisitProvider.java
+++ src/main/java/ch/meemin/minimum/provider/VisitProvider.java
@@ -15,20 +15,18 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
-import ch.meemin.minimum.entities.subscriptions.PrepaidSubscription;
-import ch.meemin.minimum.entities.subscriptions.Subscription;
-import ch.meemin.minimum.entities.subscriptions.TimeSubscription;
+import ch.meemin.minimum.entities.Visit;
 
 import com.vaadin.addon.jpacontainer.provider.MutableLocalEntityProvider;
 
 @Stateless
 @LocalBean
-public class SubscriptionProvider extends MutableLocalEntityProvider<Subscription> {
+public class VisitProvider extends MutableLocalEntityProvider<Visit> {
 	@PersistenceContext
 	private EntityManager em;
 
-	public SubscriptionProvider() {
-		super(Subscription.class);
+	public VisitProvider() {
+		super(Visit.class);
 	}
 
 	@Override
@@ -44,7 +42,7 @@ public class SubscriptionProvider extends MutableLocalEntityProvider<Subscriptio
 		setEntitiesDetached(false);
 	}
 
-	public Map<String, Long> countSoldSubscriptionsByType(Date from, Date to) {
+	public Map<String, Long> countVisitsSubscriptionType(Date from, Date to) {
 
 		from = from != null ? from : new Date(0);
 		Calendar cal = GregorianCalendar.getInstance();
@@ -59,26 +57,9 @@ public class SubscriptionProvider extends MutableLocalEntityProvider<Subscriptio
 		cal.set(Calendar.MINUTE, 59);
 		cal.set(Calendar.SECOND, 59);
 		to = cal.getTime();
-		TypedQuery<Object[]> q = em.createNamedQuery(Subscription.Q.CountByTypName, Object[].class);
+		TypedQuery<Object[]> q = em.createNamedQuery(Visit.Q.CountBySubsType, Object[].class);
 		q.setParameter("from", from);
 		q.setParameter("to", to);
-		Map<String, Long> res = new HashMap<>();
-		for (Object[] x : q.getResultList())
-			res.put((String) x[0], (Long) x[1]);
-		return res;
-	}
-
-	public Map<String, Long> countValidTimesubscriptions() {
-		TypedQuery<Object[]> q = em.createNamedQuery(TimeSubscription.Q.CountValid, Object[].class);
-		q.setParameter("date", new Date());
-		Map<String, Long> res = new HashMap<>();
-		for (Object[] x : q.getResultList())
-			res.put((String) x[0], (Long) x[1]);
-		return res;
-	}
-
-	public Map<String, Long> countValidPrepaidSubscriptions() {
-		TypedQuery<Object[]> q = em.createNamedQuery(PrepaidSubscription.Q.CountValid, Object[].class);
 		Map<String, Long> res = new HashMap<>();
 		for (Object[] x : q.getResultList())
 			res.put((String) x[0], (Long) x[1]);
